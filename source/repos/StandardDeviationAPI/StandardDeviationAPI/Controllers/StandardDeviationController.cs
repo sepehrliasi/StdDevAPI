da303754fc0476bafd458c8e3d16d7360ee2b4e1
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2025-05-30 17:25:31 +0330

    Build and run on localhost

---
+++ source/repos/StandardDeviationAPI/StandardDeviationAPI/Controllers/StandardDeviationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StandardDeviationWebAPI.Models;
+
+namespace StandardDeviationWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class StandardDeviationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalculateStandardDeviation([FromBody] PointsList request)
+        {
+            if (request.Points == null || request.Points.Count == 0)
+            {
+                return BadRequest("Data list is empty or null.");
+            }
+
+            double standardDeviation = CalculateStandardDeviation(request.Points);
+            return Ok(new { StandardDeviation = standardDeviation });
+        }
+
+        private double CalculateStandardDeviation(List<double> points)
+        {
+            double mean = points.Average();
+            double sumOfSquares = points.Sum(x => Math.Pow(x - mean, 2));
+            double variance = sumOfSquares / points.Count;
+            return Math.Sqrt(variance);
+        }
+    }
+}
---
+++ source/repos/StandardDeviationAPI/StandardDeviationAPI/Models/PointsList.cs
@@ -0,0 +1,7 @@
+﻿namespace StandardDeviationWebAPI.Models
+{
+    public class PointsList
+    {
+        public required List<double> Points { get; set; }
+    }
+}
